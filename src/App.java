b1bffe2601ef851325be6ed0abb483749bc9dbfb
allanornel <allanornel@gmail.com>
2023-01-09 19:52:40 -0300

    feat: change variable

--- src/App.java
+++ src/App.java
@@ -2,10 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         int count = 1;
         while (count < 6){
-            int var = 1;
-            while(var <= count) {
+            int checkNumber = 1;
+            while(checkNumber <= count) {
                 System.out.print(count);
-                var++;
+                checkNumber++;
             }
             System.out.println();
             count++;
